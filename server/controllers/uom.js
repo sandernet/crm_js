f1ae17bcd798d01a9ab311436e56dac57625a1ae
bobrovda <sander0net@yandex.ru>
2023-03-05 20:05:23 +0800

    Нашер решение

--- server/controllers/uom.js
+++ server/controllers/uom.js
@@ -34,6 +34,22 @@ const getOne = async (req, res) => {
     );
 };
 
+
+const del = (req, res, promiseError) => {
+    const { id } = req.body;
+  
+    if (!id) {
+      throw new Error("Not found id in body");
+    }
+  
+    model
+      .destroy({ where: { id } })
+      .then(() => {
+        res.status(200).send({ id, message: "deleted" });
+      })
+      .catch(promiseError);
+  };
+
 module.exports = (router, moduleName) => {
     model = models[moduleName];
 
@@ -42,5 +58,6 @@ module.exports = (router, moduleName) => {
     defaultHelpRouter(router, model);
     defaultPutRouter(router, moduleName, model, null);
     defaultPostRouter(router, moduleName, model, null);
-    defaultDeleteRouter(router, moduleName, model, null);
+    router.delete("/", checkMethod(del, moduleName));
+    //defaultDeleteRouter(router, moduleName, model, null);
 };
