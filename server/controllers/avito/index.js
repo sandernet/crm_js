7650c605fd9e81f7aae9d17c69c6a7f171327658
sanderO <sander_net@bk.ru>
2023-05-26 14:26:37 +0800

    Avito starting

--- server/controllers/avito/index.js
+++ server/controllers/avito/index.js
@@ -1,13 +1,5 @@
-const { checkMethod } = require("../../utils");
-const { creatXml } = require("../avito/createXml")
+const { createXml } = require("./createXml")
 
-// Плучение данных
-// const get = (req, res) => {
-//     const data = getAssortment()
-//     res.status(200).send(data);
-// };
-
-module.exports = (router, moduleName) => {
-    //router.post("/", checkMethod(post, moduleName));
-    router.get("/", checkMethod(creatXml, moduleName));
+module.exports = {
+    createXml
 }
